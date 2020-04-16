af74ed1262426e76bdb7d7a44f86a98d95ea768a
MariamSarwat <sarwatmariam@gmail.com>
2020-04-15 20:04:18 -0400

    creation of movieEmp and MovieWin

---
+++ Cadriciel/INF3710_TP4-master/common/tables/MovieWin.ts
@@ -0,0 +1,10 @@
+export interface MovieWin {
+    "id_ceremonie": number;
+    "maitre": string;
+    "nom_edifice": string;
+    "ville": string;
+    "pays": string;
+    "date_ceremonie": string;
+    "film_gagne": number;
+    "categorie_gagne": string; 
+}
